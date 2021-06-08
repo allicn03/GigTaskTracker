3ff7d4e3d620375ebf217aeb6fe8b5e1d03c5537
allicn03 <a.neusch@gmail.com>
2021-06-08 18:39:54 -0400

    first commit

---
+++ src/main/java/org/perscholas/gigtasktracker/GigTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.gigtasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigTaskTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GigTaskTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/perscholas/gigtasktracker/GigTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.gigtasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigTaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
